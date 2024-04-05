ccee68f9551e7b969e46adaa53ff0b57fcc45d3b
Grace Mwangi <neemah.mwangi@gmail.com>
2024-04-05 22:06:55 +0300

    practice type of

--- wk2-JS/index.js
+++ wk2-JS/index.js
@@ -1,6 +1,12 @@
-const fnum=10;
+ const fnum=10;
 const snum=100;
 const addition=fnum+snum;
 console.log("the sum of 2 numbers is ", addition);
+console.log("the data type is", typeof addition)
+
+const name="Grace"
+const surname="karoki"
+const fullname= name + " "+ surname
+console.log("My name is ", fullname)
 
 
