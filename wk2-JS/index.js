fb92e6abeef7eb3296952d43221daf70ee1db0d9
Grace Mwangi <neemah.mwangi@gmail.com>
2024-04-07 12:25:00 +0300

    remove unwanted server.js file

--- wk2-JS/index.js
+++ wk2-JS/index.js
@@ -2,11 +2,11 @@
 const snum=100;
 const addition=fnum+snum;
 console.log("the sum of 2 numbers is ", addition);
-console.log("the data type is", typeof addition)
+console.log("The data type is", typeof addition);
 
-const name="Grace"
-const surname="karoki"
-const fullname= name + " "+ surname
-console.log("My name is ", fullname)
+const name="Grace";
+const surname="karoki";
+const fullname= name + " "+ surname;
+console.log("My name is ", fullname);
 
 
